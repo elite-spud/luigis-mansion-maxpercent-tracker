478f2f7f127c62c8cbfa55df9877956512d0b3dc
Elite Spud <39657155+elite-spud@users.noreply.github.com>
2025-12-10 14:17:27 -0500

    split checks by floor

--- script/checks.js
+++ script/checks.js
@@ -1,500 +1,425 @@
-const checks = [
-    {
-        name: "Bucket",
-        x: "85.0%",
-        y: "76.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 1",
-        x: "93.0%",
-        y: "83.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 2",
-        x: "93.0%",
-        y: "86.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 3",
-        x: "93.0%",
-        y: "89.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 4",
-        x: "98.0%",
-        y: "83.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 5",
-        x: "98.0%",
-        y: "86.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Shelf 6",
-        x: "98.0%",
-        y: "89.0%",
-        state: CheckState.Available,
-    },
+const firstFloorChecks = [
     {
         name: "Gold Mouse",
         x: "82.5%",
         y: "38.5%",
-        state: CheckState.Available,
     },
     {
         name: "Vase",
         x: "65.0%",
         y: "38.5%",
-        state: CheckState.Available,
     },
     {
         name: "Vase",
         x: "87.0%",
         y: "38.5%",
-        state: CheckState.Available,
     },
     {
         name: "Vase",
         x: "84.0%",
         y: "20.5%",
-        state: CheckState.Available,
     },
     {
         name: "Vase",
         x: "75.0%",
         y: "20.5%",
-        state: CheckState.Available,
     },
     {
         name: "Chandelier",
         x: "87.0%",
         y: "32.0%",
-        state: CheckState.Available,
     },
     {
         name: "Chandelier",
         x: "93.0%",
         y: "32.0%",
-        state: CheckState.Available,
     },
     {
         name: "Bucket",
         x: "91.0%",
         y: "24.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "67.0%",
         y: "20.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "64.5%",
         y: "20.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "96.5%",
         y: "38.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "75.3%",
         y: "40.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Chandelier",
-        x: "25.2%",
-        y: "82.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "End Table",
-        x: "29.5%",
-        y: "86.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Cabinet",
-        x: "21.5%",
-        y: "81.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Chandelier",
-        x: "27.2%",
-        y: "76.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Chandelier",
-        x: "23.3%",
-        y: "76.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "End Table",
-        x: "21.5%",
-        y: "75.3%",
-        state: CheckState.Available,
-    },
-    {
-        name: "End Table",
-        x: "25.2%",
-        y: "75.3%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Wardrobe",
-        x: "16.5%",
-        y: "75.3%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "16.0%",
-        y: "83.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "End Table",
-        x: "4.5%",
-        y: "85.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Dresser",
-        x: "10.0%",
-        y: "85.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "5.5%",
-        y: "95.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "93.5%",
-        y: "74.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "88.5%",
         y: "42.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "83.0%",
         y: "42.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "54.5%",
         y: "38.7%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "56.5%",
         y: "38.7%",
-        state: CheckState.Available,
     },
     {
         name: "Bucket",
         x: "58.5%",
         y: "38.7%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "53.5%",
         y: "42.0%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "56.0%",
         y: "43.5%",
-        state: CheckState.Available,
     },
     {
         name: "Goblet",
         x: "61.5%",
         y: "45.0%",
-        state: CheckState.Available,
     },
     {
         name: "Goblet",
         x: "68.0%",
         y: "45.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Dresser",
-        x: "31.0%",
-        y: "68.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "76.5%",
         y: "17.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "81.0%",
         y: "17.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "79.0%",
         y: "32.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "59.0%",
         y: "33.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "64.0%",
         y: "32.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "88.0%",
         y: "16.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "85.5%",
         y: "17.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "42.5%",
-        y: "72.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Chandelier",
-        x: "38.0%",
-        y: "68.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Vase",
-        x: "34.5%",
-        y: "72.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Vase",
-        x: "31.5%",
-        y: "72.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Vase",
-        x: "23.0%",
-        y: "72.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Vase",
-        x: "40.5%",
-        y: "90.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "26.5%",
-        y: "68.5%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "15.5%",
-        y: "72.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Cabinet",
-        x: "34.5%",
-        y: "75.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Cabinet",
-        x: "42.5%",
-        y: "75.0%",
-        state: CheckState.Available,
     },
     {
         name: "Drawer",
         x: "64.0%",
         y: "24.0%",
-        state: CheckState.Available,
     },
     {
         name: "Drawer",
         x: "63.5%",
         y: "27.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "79.5%",
         y: "27.0%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "77.7%",
         y: "26.0%",
-        state: CheckState.Available,
+    }
+];
+
+const secondFloorChecks = [
+    {
+        name: "Chandelier",
+        x: "25.2%",
+        y: "82.5%",
+    },
+    {
+        name: "End Table",
+        x: "29.5%",
+        y: "86.0%",
+    },
+    {
+        name: "Cabinet",
+        x: "21.5%",
+        y: "81.0%",
+    },
+    {
+        name: "Chandelier",
+        x: "27.2%",
+        y: "76.5%",
+    },
+    {
+        name: "Chandelier",
+        x: "23.3%",
+        y: "76.5%",
+    },
+    {
+        name: "End Table",
+        x: "21.5%",
+        y: "75.3%",
+    },
+    {
+        name: "End Table",
+        x: "25.2%",
+        y: "75.3%",
+    },
+    {
+        name: "Wardrobe",
+        x: "16.5%",
+        y: "75.3%",
+    },
+    {
+        name: "Light",
+        x: "16.0%",
+        y: "83.5%",
+    },
+    {
+        name: "End Table",
+        x: "4.5%",
+        y: "85.5%",
+    },
+    {
+        name: "Dresser",
+        x: "10.0%",
+        y: "85.5%",
+    },
+    {
+        name: "Light",
+        x: "5.5%",
+        y: "95.0%",
+    },
+    {
+        name: "Light",
+        x: "42.5%",
+        y: "72.0%",
+    },
+    {
+        name: "Chandelier",
+        x: "38.0%",
+        y: "68.5%",
+    },
+    {
+        name: "Vase",
+        x: "34.5%",
+        y: "72.0%",
+    },
+    {
+        name: "Vase",
+        x: "31.5%",
+        y: "72.0%",
+    },
+    {
+        name: "Vase",
+        x: "23.0%",
+        y: "72.0%",
+    },
+    {
+        name: "Vase",
+        x: "40.5%",
+        y: "90.0%",
+    },
+    {
+        name: "Light",
+        x: "26.5%",
+        y: "68.5%",
+    },
+    {
+        name: "Light",
+        x: "15.5%",
+        y: "72.0%",
+    },
+    {
+        name: "Cabinet",
+        x: "34.5%",
+        y: "75.0%",
+    },
+    {
+        name: "Cabinet",
+        x: "42.5%",
+        y: "75.0%",
     },
     {
         name: "Dresser",
         x: "12.0%",
         y: "93.5%",
-        state: CheckState.Available,
     },
     {
         name: "Light",
         x: "15.0%",
         y: "95.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "46.0%",
-        y: "60.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "42.0%",
-        y: "60.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "20.0%",
-        y: "58.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "21.0%",
-        y: "48.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "29.0%",
-        y: "48.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Light",
-        x: "9.0%",
-        y: "48.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Jar 1",
-        x: "5.0%",
-        y: "45.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Jar 3",
-        x: "9.0%",
-        y: "45.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Jar 4",
-        x: "11.0%",
-        y: "45.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Jar 5",
-        x: "5.0%",
-        y: "51.0%",
-        state: CheckState.Available,
-    },
-    {
-        name: "Jar 8",
-        x: "11.0%",
-        y: "51.0%",
-        state: CheckState.Available,
     },
     {
         name: "Gold Mouse",
         x: "37.5%",
         y: "81.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cup",
         x: "34.5%",
         y: "81.0%",
-        state: CheckState.Available,
     },
     {
         name: "Cup",
         x: "45.0%",
         y: "80.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cup",
         x: "45.5%",
         y: "83.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cup",
         x: "46.0%",
         y: "86.5%",
-        state: CheckState.Available,
     },
     {
         name: "Chandelier",
         x: "40.5%",
         y: "83.5%",
-        state: CheckState.Available,
     },
     {
         name: "Cabinet",
         x: "48.0%",
         y: "96.5%",
-        state: CheckState.Available,
     }
 ];
+
+const thirdFloorChecks = [
+    
+    {
+        name: "Light",
+        x: "46.0%",
+        y: "60.0%",
+    },
+    {
+        name: "Light",
+        x: "42.0%",
+        y: "60.0%",
+    },
+    {
+        name: "Light",
+        x: "20.0%",
+        y: "58.0%",
+    },
+    {
+        name: "Light",
+        x: "21.0%",
+        y: "48.0%",
+    },
+    {
+        name: "Light",
+        x: "29.0%",
+        y: "48.0%",
+    },
+    {
+        name: "Light",
+        x: "9.0%",
+        y: "48.0%",
+    },
+    {
+        name: "Jar 1",
+        x: "5.0%",
+        y: "45.0%",
+    },
+    {
+        name: "Jar 3",
+        x: "9.0%",
+        y: "45.0%",
+    },
+    {
+        name: "Jar 4",
+        x: "11.0%",
+        y: "45.0%",
+    },
+    {
+        name: "Jar 5",
+        x: "5.0%",
+        y: "51.0%",
+    },
+    {
+        name: "Jar 8",
+        x: "11.0%",
+        y: "51.0%",
+    },
+    
+];
+
+const basementChecks = [
+    {
+        name: "Bucket",
+        x: "85.0%",
+        y: "76.0%",
+    },
+    {
+        name: "Shelf 1",
+        x: "93.0%",
+        y: "83.0%",
+    },
+    {
+        name: "Shelf 2",
+        x: "93.0%",
+        y: "86.0%",
+    },
+    {
+        name: "Shelf 3",
+        x: "93.0%",
+        y: "89.0%",
+    },
+    {
+        name: "Shelf 4",
+        x: "98.0%",
+        y: "83.0%",
+    },
+    {
+        name: "Shelf 5",
+        x: "98.0%",
+        y: "86.0%",
+    },
+    {
+        name: "Shelf 6",
+        x: "98.0%",
+        y: "89.0%",
+    },
+    {
+        name: "Light",
+        x: "93.5%",
+        y: "74.5%",
+    }
+];
+
+const checks = basementChecks.concat(firstFloorChecks).concat(secondFloorChecks).concat(thirdFloorChecks);
