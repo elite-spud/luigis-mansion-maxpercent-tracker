57cd60b6aaec8786894060a137a3eeb36d1fcd11
Elite Spud <39657155+elite-spud@users.noreply.github.com>
2025-12-10 12:40:49 -0500

    convert indentation to spaces

--- script/main.js
+++ script/main.js
@@ -4,53 +4,53 @@ function getCheckId(checkIndex) {
 
 // Event of clicking a check on the map
 function toggleCheck(checkIndex) {
-	// Do not allow toggling checks with saved states beyond the maxToggleableState
+    // Do not allow toggling checks with saved states beyond the maxToggleableState
     // TODO: allow retoggling of saved checks with a new control button
-	if (checks[checkIndex].state > maxToggleableState) {
-		return;
-	}
+    if (checks[checkIndex].state > maxToggleableState) {
+        return;
+    }
 
-	// State is at last option in order
-	if (checks[checkIndex].state === maxToggleableState) {
-		checks[checkIndex].state = minToggleableState;
-	} else {
-		checks[checkIndex].state++;
-	}
+    // State is at last option in order
+    if (checks[checkIndex].state === maxToggleableState) {
+        checks[checkIndex].state = minToggleableState;
+    } else {
+        checks[checkIndex].state++;
+    }
 
-	refreshCheck(checkIndex);
+    refreshCheck(checkIndex);
 }
 
 function saveChecks(){
-	for (let i = 0; i < checks.length; i++){
-		if (checks[i].state === CheckState.Collected){
-			checks[i].state = CheckState.Saved;
-		}
-		refreshCheck(i);
-	}
+    for (let i = 0; i < checks.length; i++){
+        if (checks[i].state === CheckState.Collected){
+            checks[i].state = CheckState.Saved;
+        }
+        refreshCheck(i);
+    }
 }
 
 function loadChecks(){
-	for (let i = 0; i < checks.length; i++){
-		if (checks[i].state == 1){
-			checks[i].state = defaultCheckState;
-		}
-		refreshCheck(i);
-	}
+    for (let i = 0; i < checks.length; i++){
+        if (checks[i].state == 1){
+            checks[i].state = defaultCheckState;
+        }
+        refreshCheck(i);
+    }
 }
 
 function getClassNameFromState(checkState) {
-	switch (checkState) {
-		case CheckState.Available:
-			return 'available';
-		case CheckState.Collected:
-			return 'collected';
-		case CheckState.Possible:
-			return 'possible';
-		case CheckState.Saved:
-			return 'saved';
-	}
+    switch (checkState) {
+        case CheckState.Available:
+            return 'available';
+        case CheckState.Collected:
+            return 'collected';
+        case CheckState.Possible:
+            return 'possible';
+        case CheckState.Saved:
+            return 'saved';
+    }
 
-	throw new Error(`checkState: ${checkState} not recognized as valid checkState`);
+    throw new Error(`checkState: ${checkState} not recognized as valid checkState`);
 }
 
 function refreshCheck(checkIndex) {
@@ -78,19 +78,19 @@ function createControlButton(id, hoverText, color, image, left, top, onclick) {
 
 function createButtonForMap(id, className, hoverText, color, image, left, top, onclick) {
     const span = document.createElement('span');
-	span.style.backgroundImage = image;
-	span.style.color = color;
-	span.id = id;
-	span.onclick = onclick;
-	span.onmouseover = new Function(`highlight("${id}")`);
+    span.style.backgroundImage = image;
+    span.style.color = color;
+    span.id = id;
+    span.onclick = onclick;
+    span.onmouseover = new Function(`highlight("${id}")`);
     span.onmouseout = new Function(`unhighlight("${id}")`);
     span.style.left = left;
-	span.style.top = top;
-	span.className = className;
-	childSpan = document.createElement('span');
-	childSpan.className = 'tooltip';
-	childSpan.innerHTML = hoverText;
-	span.appendChild(childSpan);
+    span.style.top = top;
+    span.className = className;
+    childSpan = document.createElement('span');
+    childSpan.className = 'tooltip';
+    childSpan.innerHTML = hoverText;
+    span.appendChild(childSpan);
     return span;
 }
 
@@ -105,8 +105,8 @@ function populateMapdiv() {
 
     // Initialize all checks on the map
     for (k = 0; k < checks.length; k++) {
-		const stateClassName = getClassNameFromState(checks[k].state);
-		const className = `mapspan check ${stateClassName}`;
+        const stateClassName = getClassNameFromState(checks[k].state);
+        const className = `mapspan check ${stateClassName}`;
         const checkButton = createButtonForMap(getCheckId(k), className, checks[k].name, "black", 'url(images/poi.png)', getMapLocation(checks[k].x), getMapLocation(checks[k].y), new Function(`toggleCheck("${k}")`));
         mapdiv.appendChild(checkButton);
     }
@@ -117,11 +117,11 @@ function populateMapdiv() {
     const controlButton4LocationLeft = "82.0%";
     const controlButtonLocationTop = "95.0%";
 
-	const saveButton = createControlButton("save-span", "Save", "black", "url(images/save.png)", controlButton1LocationLeft, controlButtonLocationTop, new Function('saveChecks()'));
-	const loadButton = createControlButton("load-span", "Load", "black", "url(images/load.png)", controlButton2LocationLeft, controlButtonLocationTop, new Function('loadChecks()'));
-	const ntscButton = createControlButton("ntsc-span", "NTSC Mode", "black", "url(images/ntsc.png)", controlButton3LocationLeft, controlButtonLocationTop, new Function('toggleVersion()'));
-	const palButton = createControlButton("pal-span", "PAL Mode", "black", "url(images/pal.png)", controlButton3LocationLeft, "95.0%", new Function('toggleVersion()'));
-	const altDisplayButton = createControlButton("alt-display-span", "Alt Display", "black", "url(images/alt.png)", controlButton4LocationLeft, "95.0%", new Function('toggleAltDisplay()'));
+    const saveButton = createControlButton("save-span", "Save", "black", "url(images/save.png)", controlButton1LocationLeft, controlButtonLocationTop, new Function('saveChecks()'));
+    const loadButton = createControlButton("load-span", "Load", "black", "url(images/load.png)", controlButton2LocationLeft, controlButtonLocationTop, new Function('loadChecks()'));
+    const ntscButton = createControlButton("ntsc-span", "NTSC Mode", "black", "url(images/ntsc.png)", controlButton3LocationLeft, controlButtonLocationTop, new Function('toggleVersion()'));
+    const palButton = createControlButton("pal-span", "PAL Mode", "black", "url(images/pal.png)", controlButton3LocationLeft, "95.0%", new Function('toggleVersion()'));
+    const altDisplayButton = createControlButton("alt-display-span", "Alt Display", "black", "url(images/alt.png)", controlButton4LocationLeft, "95.0%", new Function('toggleAltDisplay()'));
 
     mapdiv.appendChild(saveButton);
     mapdiv.appendChild(loadButton);
@@ -131,16 +131,16 @@ function populateMapdiv() {
 }
 
 function handleKeydown(e){
-	const key = e.keyCode;
-	switch (key)
-	{
-		case KeyCodes.LoadKey:
-			loadChecks();
-			break;
-		case KeyCodes.SaveKey:
-			saveChecks();
-			break;
-	}
+    const key = e.keyCode;
+    switch (key)
+    {
+        case KeyCodes.LoadKey:
+            loadChecks();
+            break;
+        case KeyCodes.SaveKey:
+            saveChecks();
+            break;
+    }
 }
 
 function init() {
--- script/settings.js
+++ script/settings.js
@@ -1,59 +1,59 @@
 /*
-	gameVersion tracks what version of the game is being tracked (NTSC vs. PAL).
-	This is helpful because the PAL hidden mansion is mirrored compared to the NTSC version.
+    gameVersion tracks what version of the game is being tracked (NTSC vs. PAL).
+    This is helpful because the PAL hidden mansion is mirrored compared to the NTSC version.
 */
 
 const GameVersion = {
-	NTSC: "ntsc",
-	PAL: "pal"
+    NTSC: "ntsc",
+    PAL: "pal"
 }
 const currentGameVersion = GameVersion.NTSC;
 
 /*
-	defaultCheckState is the initial state of money spots when loading the money tracker.
-	0 = available / gray
-	1 = collected / yellow
-	2 = possible / red
-	3 = saved / green 
-	
-	Saving (or setting defaultCheckState to 3) will disallow changing of that money location, 
-	so don't set defaultCheckState to 3.
-	
-	Possible locations don't get saved, so you can remember them later if need be.
-	
-	Note: if you want to change the default color for each of these states,
-	you can do so by modifying the following background-color RGB Value in style.css:
+    defaultCheckState is the initial state of money spots when loading the money tracker.
+    0 = available / gray
+    1 = collected / yellow
+    2 = possible / red
+    3 = saved / green 
+    
+    Saving (or setting defaultCheckState to 3) will disallow changing of that money location, 
+    so don't set defaultCheckState to 3.
+    
+    Possible locations don't get saved, so you can remember them later if need be.
+    
+    Note: if you want to change the default color for each of these states,
+    you can do so by modifying the following background-color RGB Value in style.css:
 
-	.available
-	.collected
-	.possible
-	.saved
+    .available
+    .collected
+    .possible
+    .saved
 */
 
 const CheckState = {
-	Available: 0,
-	Collected: 1,
-	Possible: 2,
-	Saved: 3,
+    Available: 0,
+    Collected: 1,
+    Possible: 2,
+    Saved: 3,
 }
 const defaultCheckState = CheckState.Available;
 
 /*
-	maxToggleableState limits how high the checkState can be set via toggling.
-	Values higher than this must be set in other ways (e.g. via saving).
-	Saved states above this value cannot be changed via toggling.
+    maxToggleableState limits how high the checkState can be set via toggling.
+    Values higher than this must be set in other ways (e.g. via saving).
+    Saved states above this value cannot be changed via toggling.
 */
 
 const maxToggleableState = 2;
 const minToggleableState = 0;
 
 /*
-	The keys listed below are ascii values, if you wish to change these you can easily 
-	find codes for whatever keys you'd like here: (use the event.which number) 
-	https://www.toptal.com/developers/keycode 
+    The keys listed below are ascii values, if you wish to change these you can easily 
+    find codes for whatever keys you'd like here: (use the event.which number) 
+    https://www.toptal.com/developers/keycode 
 */
 
 const KeyCodes = {
-	SaveKey: 32, // spacebar
-	LoadKey: 27 // escape
+    SaveKey: 32, // spacebar
+    LoadKey: 27 // escape
 }
